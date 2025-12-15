37f4d229434137361922b066ebd416aaac129758
vinzz-officials <vinzz4358@gmail.com>
2025-12-15 19:42:42 +0800

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const r = await fetch(
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
-      process.env.GEMINI_API_KEY,
+      process.env.gemini,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
